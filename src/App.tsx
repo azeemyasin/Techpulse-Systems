d745e3c9ea51320dd438bc0f72b5f4050e512939
Mudassar-Iftikhar <mianmudassar9900@gmail.com>
2024-06-23 07:07:51 +0500

    coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-black relative">
+        <img
+          className="h-screen w-full object-contain"
+          src="misc/placeholder.png"
+          alt=""
+        />
+        <div className="sm:w-56 w-40 sm:h-56 h-40 bg-[#F7F7F7] rounded-[0_0_30%_0] absolute top-0 left-0 flex items-center justify-center">
+        <img src="/logo-white.jpeg" alt="" className="w-11/12" />
+        </div>
+      </div>
+    </>
   );
 }
 
